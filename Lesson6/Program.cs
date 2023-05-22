c06e222b4fa0a95cf2f1e28fbe441367fe17edf3
Mikhail Sitnikov <misha.sitnikov.97@mail.ru>
2023-05-22 22:18:17 +0400

    почти рабочая версия

--- Lesson6/Program.cs
+++ Lesson6/Program.cs
@@ -14,10 +14,6 @@ namespace Lesson6
         {
             using (StreamWriter sw = new StreamWriter("Cотрудники.txt", true, Encoding.Unicode))
             {
-                char add = '2';
-                char open = '1';
-                do
-                {
                     string note = String.Empty;
 
                     uint id = 1;
@@ -27,22 +23,20 @@ namespace Lesson6
                     string dateTime = DateTime.Now.ToString();
                     note += $"{dateTime}#";
 
-                    Console.WriteLine("Введите ФИО: ");
+                    Console.Write("Введите ФИО: ");
                     note += $"{Console.ReadLine()}#";
 
-                    Console.WriteLine("Введите возраст: ");
+                    Console.Write("Введите возраст: ");
                     note += $"{Console.ReadLine()}#";
 
-                    Console.WriteLine("Введите рост: ");
+                    Console.Write("Введите рост: ");
                     note += $"{Console.ReadLine()}#";
 
-                    Console.WriteLine("Введите дату рождение в формате ДД.ММ.ГГГГ: ");
+                    Console.Write("Введите дату рождение в формате ДД.ММ.ГГГГ: ");
                     note += $"{Console.ReadLine()}#";
 
-                    Console.WriteLine("Введите место рождения(город): ");
+                    Console.Write("Введите место рождения(город): ");
                     note += $"город {Console.ReadLine()}\t";
-                }
-                while (char.ToLower(add) == '2');
             }
         }
 
@@ -51,17 +45,39 @@ namespace Lesson6
             using (StreamReader sr = new StreamReader("Cотрудники.txt", Encoding.Unicode))
             {
                 string line;
-                while ((line = sr.ReadLine()) != null)
+                if ((line = sr.ReadLine()) != null)
                 {
-                    string[] data = line.Split('#');
-                    Console.WriteLine($"{data[0],15}.{data[1],15}{data[2],15}{data[3],15}лет{data[4],15}см.{data[5],15}г.р.{data[6],15}");
+                    while ((line != null))
+                    {
+                        string[] data = line.Split('#');
+                        Console.WriteLine($"{data[0],15}.{data[1],15}{data[2],15}{data[3],15}лет{data[4],15}см.{data[5],15}г.р.{data[6],15}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Файл не создан/пустой. Нажмите 2 для создания файла и внесения в нее первой записи");
                 }
             }
         }
 
         static void Main(string[] args)
         {
-
+            char key = '0';
+            
+            while (true)
+            {
+                Console.WriteLine("\tНажмите 1, чтобы отобразить список. Нажмите 2, чтобы внести новые данные");
+                Console.ReadKey();
+                switch (key)
+                {
+                    case '1':
+                        Reader(); break;
+                    case '2':
+                        Writer(); break;
+                        
+                }
+            }
+           // char.ToLower(add) == '1'
         }
     }
 }
